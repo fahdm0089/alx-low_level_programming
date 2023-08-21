8efef6563e827e88a8f1ebba902d47842f9c70d0
fahdm0089 <fahdm7693@gmail.com>
2023-08-21 08:53:20 -0700

    a

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to int parametr
+ * and updates the values it points to 98
+ *
+ * @n: int parametr
+ *
+ * Return: nothing
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
