778883b86c57bb11675944b1ae09f2e48cd1b80e
fahdm0089 <fahdm7693@gmail.com>
2023-08-30 14:55:58 -0700

    a

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -27,7 +27,7 @@ int is_palindrome(char *s)
 {
 	int end = last_index(s);
 
-	return (checks(s, 0, end - 1, end % 2));
+	return (check(s, 0, end - 1, end % 2));
 }
 /**
  * check - checker for the palindrome
