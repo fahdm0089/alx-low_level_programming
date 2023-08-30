8ff02d6516e8820a9922450ae8b070eef89ef9e9
fahdm0089 <fahdm7693@gmail.com>
2023-08-30 07:29:21 -0700

    a

--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -1,11 +1,11 @@
-#include <unistd>
+#include <unistd.h>
 
 /**
  * _putchar - writes the charchter c to stdout
- * @c: tthe charchter to print
+ * @c: the charchter to print
  *
  * Return: on success 1.
- * on error, -1 is returned, and erno is set appropriately.
+ * on error, -1 is returned and errno is set appropriately
 */
 int _putchar(char c)
 {
